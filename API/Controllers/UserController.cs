97f1b63a8db21db262054cd6a8a27449688054e7
Jitesh200 <l.sreejitesh2000@gmail.com>
2023-10-10 12:48:38 +0530

    Part 1

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using API.Data;
+using API.Entity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SQLitePCL;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UserController:ControllerBase
+{
+    private readonly DataContext _context;
+  public UserController(DataContext context)
+  {
+    this._context=context;
+  }
+  [HttpGet]
+  public async Task< ActionResult<IEnumerable<ApplicationUser>>> GetUsers()
+  {
+    var users= await _context.Users.ToListAsync();
+
+    return users;
+
+  }
+
+  [HttpGet("{id}")]
+  public async Task<ActionResult<ApplicationUser>> GetUser(int id)
+  {
+    return await _context.Users.FindAsync(id);
+  }
+
+
+}
